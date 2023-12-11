94ad95c7fa4b548ce00c6e63e24209fe758994ac
carregando <joojnow@gmail.com>
2023-12-10 20:16:40 -0400

    Exemplo de components e props

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
+import Title from '../src/components/title/Title'
+
 export default function HomePage () {
     return (
         <>
          <div>
-            Página Inicial
+            <Title>Titulo fa página</Title>
          </div>
         </>
     )
